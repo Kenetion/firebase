98e913d38acf18d52afd32a863661b95ffed8af1
Kenetion <wojtekforum@gmail.com>
2023-03-29 21:37:49 +0200

    added text in home page and css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,57 +1,21 @@
+import '../App.css';
+
+
+
 export const Home = () => {
     return (
     <div>
-        <h1>THIS IS HOME PAGE THIS IS HOME PAGE THIS IS HOME PAGE THIS IS HOME PAGE THIS IS HOME PAGE THIS IS HOME PAGE </h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
-        <h1>THIS IS HOME PAGE</h1>
+        <p className="parag">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam consectetur illo esse dolor hic vitae adipisci repellat expedita accusantium rerum, pariatur natus debitis. Error animi, similique nulla laborum est provident?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores minus ab quod, aliquid architecto asperiores officia labore porro fuga laborum voluptatibus reiciendis iste ipsa tempore. Necessitatibus ad eum aperiam? Voluptates!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, illo facilis sequi deleniti unde delectus quo debitis numquam sed facere nihil maiores odit vel recusandae earum enim cumque molestiae! Modi!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vel culpa nobis repellendus cum optio at porro ipsam sint illo? Veniam eum, ullam vel aut itaque debitis quasi reiciendis sit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam mollitia provident officia necessitatibus ipsam aut quis quaerat quasi, tenetur repudiandae rerum expedita tempora molestias accusamus iure quam ipsum ea nam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quo laboriosam, ipsum eum est incidunt rem corporis voluptate, dolorum, minima quos suscipit fugiat a dolor fugit veniam cumque perferendis alias!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro eum provident tenetur. Laudantium eveniet illum assumenda nesciunt corporis, est corrupti et numquam placeat amet repudiandae culpa iure repellat nostrum deserunt.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. In expedita libero eum saepe accusantium atque autem quod recusandae ea assumenda! Repellendus maiores beatae impedit atque, dignissimos ad voluptas magnam modi?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur autem earum, rem fuga veniam sed asperiores, doloribus, nisi voluptate unde debitis vel hic? Deserunt id magni qui veniam, libero corrupti!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. At placeat harum suscipit! Magni accusamus deserunt eius atque, ipsam, totam doloribus omnis ut quaerat vel reiciendis inventore, similique aliquam voluptatibus aperiam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius doloremque totam, recusandae placeat labore aliquam blanditiis commodi facere quibusdam nemo aut enim obcaecati suscipit odit, ex maiores natus veritatis saepe! </p>
     </div>
     );
 };
